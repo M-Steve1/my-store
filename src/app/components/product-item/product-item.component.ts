99513563b94317b92150c36bbc3cae7167c19db7
Modebe Stephen <modebestephen2@gmail.com>
2023-01-31 13:15:02 +0100

    Set up product-item.component.ts

--- src/app/components/product-item/product-item.component.ts
+++ src/app/components/product-item/product-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from 'src/app/models/Product';
 
 @Component({
   selector: 'app-product-item',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-item.component.css']
 })
 export class ProductItemComponent {
-
+  @Input() product: Product = {
+    name: '',
+    price: 0,
+    url: '',
+    description: ''
+  }
 }
