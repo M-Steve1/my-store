0a75f83744d65e2f2ee1c8133ac8325eda876dde
Modebe Stephen <modebestephen2@gmail.com>
2023-01-31 13:06:00 +0100

    Setup product service

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Product } from '../models/Product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  apiUrl: string = 'http://localhost:8000/products';
 
   constructor(private http: HttpClient) {}
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.apiUrl);
+  }
 }
