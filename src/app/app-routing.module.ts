7e1c85483ba8e2db72a35cf34b64ef16510cdd2d
Modebe Stephen <modebestephen2@gmail.com>
2023-02-01 20:17:48 +0100

    Setup confirmation component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProductItemDetailComponent } from './components/product-item-detail/product-item-detail.component';
+import { ConfirmationComponent } from './components/confirmation/confirmation.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 
 const routes: Routes = [
   {path: '', component: ProductListComponent},
-  {path: 'product-item-detail', component: ProductItemDetailComponent}
+  {path: 'confirmation', component: ConfirmationComponent}
 ];
 
 @NgModule({
