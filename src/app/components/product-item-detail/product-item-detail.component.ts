ab2c32b9d9bb9c626de63d0c7615c56b8f533daf
Modebe Stephen <modebestephen2@gmail.com>
2023-02-03 21:59:13 +0100

    Setup form functionalities for product-item-detail component

--- src/app/components/product-item-detail/product-item-detail.component.ts
+++ src/app/components/product-item-detail/product-item-detail.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Product } from 'src/app/models/Product';
 import { UiService } from 'src/app/services/ui.service';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { Order } from 'src/app/models/Order';
 
 @Component({
   selector: 'app-product-item-detail',
@@ -17,10 +18,31 @@ export class ProductItemDetailComponent {
     description: ''
   }
 
+  @Input() product: Product = {
+    name: '',
+    price: 0,
+    url: '',
+    description: ''
+  }
+  quantity: number = 1;
+
+  @Output() createOrder: EventEmitter<Order> = new EventEmitter()
+
   constructor(private uiService: UiService) {
   }
 
   gotoProductList(): void {
     this.uiService.hideProduct();
+  }
+
+  onCreateOrder(product: Product): void {
+    const order: Order = {
+      productId: product.id as unknown as string,
+      quantity: parseInt(this.quantity as unknown as string)
+    }
+
+    this.createOrder.emit(order);
+
+    this.quantity = 1;
   }
 }
